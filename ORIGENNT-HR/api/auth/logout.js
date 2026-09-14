c532b81c6bb11513585ff12e85faccb907c261df
Debargha Chakraborty <debarghachakrabortyofficial@gmail.com>
2026-09-15 05:06:21 +0530

    HR: add logout endpoint

--- ORIGENNT-HR/api/auth/logout.js
+++ ORIGENNT-HR/api/auth/logout.js
@@ -1,1 +1,21 @@
 
+const { buildClearCookie } = require('../_lib/session');
+
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      success: false,
+      error: 'Method not allowed.'
+    });
+  }
+
+  res.setHeader(
+    'Set-Cookie',
+    buildClearCookie()
+  );
+
+  return res.status(200).json({
+    success: true,
+    message: 'Signed out.'
+  });
+}
